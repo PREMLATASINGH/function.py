a9b497b8573797c7a1d3b21f505efa3fa0aaf553
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-24 14:54:14 -0500

    added temp

---
+++ temprature.py
@@ -0,0 +1,8 @@
+def temp_convert(value, unit):
+    if unit == "C":
+        return (value * 9/5) + 32
+    elif unit == "F":
+        return (value - 32) * 5/9
+    else:
+        return "Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit."
+print(temp_convert(25, "C"))
