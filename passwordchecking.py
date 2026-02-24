c34eeb0b60c78332e68011b062671c1380abea17
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-24 14:58:32 -0500

    added

---
+++ passwordchecking.py
@@ -0,0 +1,13 @@
+def is_strong_password(password):
+    if len(password) < 8:
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" for char in password):
+        return False
+    return True
+print(is_strong_password("Password123!"))
