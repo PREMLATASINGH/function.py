dc760a8b10d12f66c8a208553baa2575185ca476
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-24 15:08:25 -0500

    added

---
+++ email.py
@@ -0,0 +1,8 @@
+def valide_email(email):
+    if "@" in email and "." in email:
+        return "Valid email address"
+    else:
+        return "Invalid email address"
+print(valide_email("example@example.com"))
+print(valide_email("invalidemail.com"))
+print(valide_email("another@example"))
